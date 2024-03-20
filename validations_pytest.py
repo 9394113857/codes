b3b6ebcfdcfe9ecad84a962b1117660ddfe464b9
Raghunadh Barlapudi <81347796+9394113857@users.noreply.github.com>
2024-03-20 17:51:48 +0530

    test codes

---
+++ validations_pytest.py
@@ -0,0 +1,4 @@
+import pytest
+
+from re_validations.validations import validate_email, validate_phone
+
---
+++ validations_unittest.py
@@ -0,0 +1,4 @@
+import unittest
+
+from re_validations.validations import validate_email, validate_phone
+
